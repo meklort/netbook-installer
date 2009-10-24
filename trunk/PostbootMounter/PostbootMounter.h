c216369c03fa2f83384bf7ca9f8d3b43d0113277
meklort <meklort@606e956a-65d2-11de-bb41-d1906ad598ae>
2009-10-24 01:41:53 +0000

    PostbooMounter kext. This is used to mount the /dev/md0 ramdisk after the root file system has been mounted. This is the initial version (and  can use some cleanup / fixing).

---
+++ trunk/PostbootMounter/PostbootMounter.h
@@ -0,0 +1,18 @@
+
+#include <IOKit/IOService.h>
+#include <IOKit/IOLib.h>
+
+
+
+	// class definition
+class PostbootMounter : public IOService
+{
+	OSDeclareDefaultStructors(PostbootMounter)
+	
+public:
+	
+	virtual bool	start(IOService *provider);
+		//	virtual int		mount_dev(const char* devpath, const char* mountpath, const char* fstype, int mntflags);
+		//	virtual int		exec_file(const char* path);
+
+};
